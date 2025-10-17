bc1dad50c0d8f67127bb609bad7eee855441a19e
YuxinSunnn <108762532+YuxinSunnn@users.noreply.github.com>
2025-10-17 19:39:01 +0000

    Deploying to gh-pages from @ YuxinSunnn/YuxinSunnn.github.io@cde8de40a0a1d30b22e43b5e45183a309c93bc33 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "YuxinSunnn/yuxin-website.github.io",
+    "data-repo": "YuxinSunnn/YuxinSunnn.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
